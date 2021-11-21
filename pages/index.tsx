27458484f16a4bcabe416e82c08c48cb3db9c2cf
Fernando López <ferlopezm94@gmail.com>
2021-11-20 21:55:31 -0600

    chore(prettier): add and setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   return (
@@ -17,7 +17,7 @@ const Home: NextPage = () => {
         </h1>
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
