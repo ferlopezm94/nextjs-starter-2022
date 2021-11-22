0fbcae55b34324377296a5343d56be71a8597af5
Fernando López <ferlopezm94@gmail.com>
2021-11-21 23:53:18 -0600

    chore(twin.macro): add and setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
-import 'tailwindcss/tailwind.css';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
+import GlobalStyles from './../components/GlobalStyles';
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
