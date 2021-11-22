98c5c6010f5a31bac048af050667578508903f45
Fernando López <ferlopezm94@gmail.com>
2021-11-21 22:18:35 -0600

    chore(styled-components): add and setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,10 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import styled from 'styled-components';
+
 import styles from '../styles/Home.module.css';
+
+const Title = styled.h1``;
 
 const Home: NextPage = () => {
   return (
@@ -12,9 +16,9 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <Title className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        </Title>
       </main>
     </div>
   );
